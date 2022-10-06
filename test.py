98b31776204d89042badc37c0e40a32286bec8fb
dinithif <dinithif.jkh@keells.com>
2022-10-07 01:43:55 +0530

    testing one with black

--- test.py
+++ test.py
@@ -1,25 +1,31 @@
 import logging
 import os
+
+
 def first_function():
-    print('this is first function')#this is a print statement
-#dictionary list
-temp_list = [1,
-2,
-3,
-4,
-5,
+    print("this is first function")#this is a print statement
+
+
+# dictionary list
+temp_list = [1, 
+2, 
+3, 
+4, 
+5, 
 6]
-#dictionary test
-person = {"name": "Bob",
-"age": 22,
-"country": "japan","job":"driver"
+# dictionary test
+person = {"name": "Bob", 
+"age": 22, 
+"country": "japan"
+, 
+"job": "driver"}
 
-}
 
-def demo_function(input, output,            features, num1, num2=3):
+def demo_function(input, 
+output, features, num1, num2=3):
     if input > 0:
-        print(
-            "positive input")
+        print("        positive input")
 
 
 
+        
